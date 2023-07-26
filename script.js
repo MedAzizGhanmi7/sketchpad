583e5b4d6f2c3d0fa256fc482f746a67ecdb2ed2
MedAzizGhanmi7 <ghanmimohamedaziz5@gmail.com>
2023-07-26 17:30:13 +0100

    add change grid button and the prompt popup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 let divx;
+let promptVal;
 for(let i =0 ; i<256;i++){
   divx = document.createElement('div');
   container.appendChild(divx);
@@ -10,7 +11,13 @@ const divs = document.querySelectorAll('.divSquare');
 divs.forEach(
     (di)=>{di.addEventListener('mouseover',
     ()=>{di.classList.add('hovered')}
-    );
+    ); }
+);
 
-    }
+const change = document.querySelector('#change');
+change.addEventListener('click',
+()=>{
+   promptVal =Number(prompt("enter the number of squares per side MAX 100"));
+   console.log(promptVal);
+}
 )
