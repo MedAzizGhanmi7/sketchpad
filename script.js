d828551f13775c7a9695e3cd8a8aeef66360bf2b
MedAzizGhanmi7 <ghanmimohamedaziz5@gmail.com>
2023-07-26 18:53:10 +0100

    add functionality to change grid button

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 const container = document.querySelector('.container');
-let divx;
-let promptVal;
-for(let i =0 ; i<256;i++){
-  divx = document.createElement('div');
-  container.appendChild(divx);
-  divx.classList.add("divSquare");
-}
-
-const divs = document.querySelectorAll('.divSquare');
-divs.forEach(
-    (di)=>{di.addEventListener('mouseover',
-    ()=>{di.classList.add('hovered')}
-    ); }
-);
-
 const change = document.querySelector('#change');
-change.addEventListener('click',
-()=>{
-   promptVal =Number(prompt("enter the number of squares per side MAX 100"));
-   console.log(promptVal);
+let side = 16; // Default number of squares per side
+let grid;
+
+newGrid();
+setGrid(calculate(side));
+
+function removeOldGrid() {
+  const rmv = document.querySelectorAll('.divSquare');
+  rmv.forEach((item) => {
+    item.remove();
+  });
 }
-)
+
+function newGrid() {
+  for (let i = 0; i < side * side; i++) {
+    const divx = document.createElement('div');
+    container.appendChild(divx);
+    divx.classList.add('divSquare');
+
+    divx.addEventListener('mouseover', () => {
+      divx.classList.add('hovered');
+    });
+  }
+}
+
+change.addEventListener('click', () => {
+  let promptVal = Number(prompt('Enter the number of squares per side (MAX 100)', side));
+  if (promptVal > 0 && promptVal <= 100) {
+    removeOldGrid();
+    side = promptVal;
+    newGrid();
+    grid = calculate(side);
+    setGrid(grid);
+  } else {
+    alert('Please enter a valid number between 1 and 100.');
+  }
+});
+
+function calculate(num) {
+  const w = 800;
+  return w / num;
+}
+
+function setGrid(grid) {
+  const grids = document.querySelectorAll('.divSquare');
+  grids.forEach((gr) => {
+    gr.style.height = grid + 'px';
+    gr.style.width = grid + 'px';
+  });
+}
